bf55b8bbb13c1661c10a276a394b82448ccc4e38
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-12-04 21:32:45 +0530

    cart list display settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css';
-import Item from './screens/Item'
+import CartList from './screens/CartList';
+import ItemList from './screens/ItemList';
+import { useAppSelector } from './store/hooks';
 
 function App() {
+  const cartList = useAppSelector(state => state.item.cartItems);
+  console.log("heeeee", cartList);
+
   return (
     <div className="App">
-      <Item />
+      {cartList.length > 0 ? (
+        <div className='flex flex-row'>
+          <div>
+            <ItemList />
+          </div>
+          <div className='cart-nav'>
+            <CartList cartList={cartList}/>
+          </div>
+        </div>
+      ) : (
+        <ItemList />
+      )}
     </div>
   );
 }
