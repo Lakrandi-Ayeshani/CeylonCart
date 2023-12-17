8792793bf1fa5a79be879bacde5bc26d4278dbe2
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-12-17 14:53:42 +0530

    main page view changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import CartList from './screens/CartList';
 import ItemList from './screens/ItemList';
 import { useAppSelector } from './store/hooks';
 
 function App() {
+  const [totalPrice, setTotalPrice] = useState<number>(0);
   const cartList = useAppSelector(state => state.item.cartItems);
-  console.log("heeeee", cartList);
+  console.log("cartList", cartList);
 
   return (
     <div className="App">
       {cartList.length > 0 ? (
         <div className='flex flex-row'>
           <div>
-            <ItemList />
+            <ItemList totalPrice={totalPrice} setTotalPrice={setTotalPrice} styleClass="with-cart"/>
           </div>
           <div className='cart-nav'>
-            <CartList cartList={cartList}/>
+            <CartList cartList={cartList} totalPrice={totalPrice} setTotalPrice={setTotalPrice}/>
           </div>
         </div>
       ) : (
-        <ItemList />
+        <ItemList totalPrice={totalPrice} setTotalPrice={setTotalPrice} styleClass="without-cart"/>
       )}
     </div>
   );
