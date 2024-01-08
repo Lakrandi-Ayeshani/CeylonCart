d5cdb7566e6edbb21884557bfff9786ebafae108
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2024-01-08 20:46:19 +0530

    Add header to application

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './App.css';
 import CartList from './screens/CartList';
 import ItemList from './screens/ItemList';
 import { useAppSelector } from './store/hooks';
+import icon from './docs/icon.png'
 
 function App() {
   const [totalPrice, setTotalPrice] = useState<number>(0);
   const cartList = useAppSelector(state => state.item.cartItems);
-  console.log("cartList", cartList);
 
   return (
     <div className="App">
+      <div className='flex header p-5 mb-5 mt- text-start text-slate-500 md:text-3xl text-2xl font-bold bg-slate-300 ps-12'>
+        <img src={icon} alt='icon' className='w-10 h-10 mx-5' />
+        <h1 className='text-slate-700'>Ceylon Cart</h1>
+      </div>
       {cartList.length > 0 ? (
         <div className='flex flex-row'>
           <div>
